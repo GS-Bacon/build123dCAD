f81c280e741b3cc418a8377e70335af2ba0a6471
bacon <bacon0817.ix@gmail.com>
2023-05-02 17:20:16 +0900

    polehalf

--- main.py
+++ main.py
@@ -10,24 +10,27 @@ flange_thin=1
 bearing_Cangle=36
 bearing_OR=47/2
 bearing_thin=7.5
-outpole_lengh=(pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(bearing_Cangle)+bearing_thin)
+outpole_lengh=(pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))+bearing_thin)
 warll_thin=4
 
 #ヘッドセットOrbitMX No.20に合せて
-flange=[(-0,-0.01),
-       (pole_OR,0),
-       (pole_OR,pole_lengh),
-       (flange_OR,pole_lengh),
-       (flange_OR,pole_lengh+flange_thin),
-       (bearing_OR,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))),
-       (bearing_OR,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))+bearing_thin),
-       (0,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))+bearing_thin),
+flange=[(0,-0,0),
+       (0,pole_OR,0),
+       (0,pole_OR,pole_lengh),
+       (0,flange_OR,pole_lengh),
+       (0,flange_OR,pole_lengh+flange_thin),
+       (0,bearing_OR,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))),
+       (0,bearing_OR,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))+bearing_thin),
+       (0,0,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))+bearing_thin),
         ]
 
 l1=Polyline(*flange)
-l2 =Line(l1@1,l1@0)
+line =Line(l1@1,l1@0)
 
-sk23 = make_face(l1, l2)
+face = make_face(l1, line)
+ex23=Pos(0,0,outpole_lengh/2)*Cylinder(bearing_OR+warll_thin*2,outpole_lengh)
 
-ex23 = revolve(sk23, Axis.Y)
+
+ex23 -= revolve(face, Axis.Z)
+
 show(ex23)
