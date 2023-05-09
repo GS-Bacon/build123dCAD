13d2743521c811eb062c05648d3ef5eba79c0763
bacon <bacon0817.ix@gmail.com>
2023-05-09 15:50:24 +0900

    add base

--- main.py
+++ main.py
@@ -3,36 +3,52 @@ from ocp_vscode import show, show_object, reset_show, set_port, set_defaults, ge
 from math import *
 set_port(3939)
 
-def pipe():
-        pole_lengh=120/2
-        pole_OR=34/2
-        flange_OR=40/2
-        flange_thin=1
-        bearing_Cangle=36
-        bearing_OR=47/2
-        bearing_thin=7.5
-        outpole_lengh=(pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))+bearing_thin)
-        warll_thin=4
-        #ヘッドセットOrbitMX No.20に合せて
-        flange=[(0,-0,0),
-        (0,pole_OR,0),
-        (0,pole_OR,pole_lengh),
-        (0,flange_OR,pole_lengh),
-        (0,flange_OR,pole_lengh+flange_thin),
-        (0,bearing_OR,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))),
-        (0,bearing_OR,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))+bearing_thin),
-        (0,0,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))+bearing_thin),
-        ]
+#プリント品基準の壁厚
+warll_thin=4
 
-        l1=Polyline(*flange)
-        line =Line(l1@1,l1@0)
-        face = make_face(l1, line)
+pole_lengh=120/2 #ハンドルポール長さ
+pole_OR=34/2 #hハンドルポール外径
+flange_OR=40/2
+flange_thin=1
+bearing_Cangle=36
+bearing_OR=47/2
+bearing_thin=7.5
+out_pole_lengh=(pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))+bearing_thin)
 
-        out_pole=Cylinder(bearing_OR+warll_thin*2,outpole_lengh)
-        ex23=Pos(0,0,outpole_lengh/2)*out_pole
+#ヘッドセットOrbitMX No.20に合せて回転形状を生成
+#line(X,Y,Z)
+flange=[(0,0,0),
+(0,pole_OR,0),
+(0,pole_OR,pole_lengh),
+(0,flange_OR,pole_lengh),
+(0,flange_OR,pole_lengh+flange_thin),
+(0,bearing_OR,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))),
+(0,bearing_OR,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))+bearing_thin),
+(0,0,pole_lengh+flange_thin+(bearing_OR-flange_OR)*tan(radians(bearing_Cangle))+bearing_thin),
+]
 
-        ex23 -= revolve(face, Axis.Z)
-        return ex23
+l1=Polyline(*flange)
+line =Line(l1@1,l1@0)
+face = make_face(l1, line)
 
-ex24=pipe()+mirror(pipe(),Plane.XY)
-show(ex24)
+out_pole_OR=bearing_OR+warll_thin*2 #25.5
+
+out_pole=Cylinder(out_pole_OR,out_pole_lengh)
+ex2=Pos(0,0,out_pole_lengh/2)*out_pole
+
+ex2 -= revolve(face, Axis.Z)
+
+show(ex2)
+#show(ex2)
+
+pole_base=[(-20,0,0),
+           (20,0,0),
+           (20,0,out_pole_lengh),
+           (-20,0,out_pole_lengh),
+           (-20,0,0)]
+l2=Pos(0,out_pole_OR+0.1,0)*Polygon(*pole_base)
+ex2+=extrude(l2,until=Until.NEXT,target=ex2)
+
+ex2+=mirror(ex2,Plane.XZ)
+ex2+=mirror(ex2,Plane.XY)
+show(ex2)
