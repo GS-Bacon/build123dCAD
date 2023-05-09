0e2849f12c343e9948ce4ad0251d46bd66485943
bacon <bacon0817.ix@gmail.com>
2023-05-09 13:57:14 +0900

    def

--- main.py
+++ main.py
@@ -26,11 +26,13 @@ def pipe():
 
         l1=Polyline(*flange)
         line =Line(l1@1,l1@0)
-
         face = make_face(l1, line)
-        ex23=Pos(0,0,outpole_lengh/2)*Cylinder(bearing_OR+warll_thin*2,outpole_lengh)
 
+        out_pole=Cylinder(bearing_OR+warll_thin*2,outpole_lengh)
+        ex23=Pos(0,0,outpole_lengh/2)*out_pole
 
         ex23 -= revolve(face, Axis.Z)
-        show(ex23)
-pipe()
+        return ex23
+
+ex24=pipe()+mirror(pipe(),Plane.XY)
+show(ex24)
